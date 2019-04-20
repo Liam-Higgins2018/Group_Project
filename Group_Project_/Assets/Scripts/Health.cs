6b31be9aeb2b750e082f1d30e5624002404a95e2
Liam-Higgins2018 <liammhiggins@hotmail.com>
2019-04-20 19:28:38 +0100

    Health Sprites and Class Update

---
+++ Group_Project_/Assets/Scripts/Health.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int noOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health > noOfHearts)
+        {
+            health = noOfHearts;
+        }
+
+        for(int i = 0; i < hearts.Length; i++)
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else{
+                hearts[i].sprite = emptyHeart;
+            }
+            
+            if(i < noOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+
+            else
+            {
+                 hearts[i].enabled = false;
+            }
+        }
+    }
+}
