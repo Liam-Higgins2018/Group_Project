ea79e4bab081fb1507b4f5833edb4456fdc0494c
Jaymz95 <jamesmullarkey15@gmail.com>
2019-04-24 00:12:12 +0100

    Character cant leave the track

--- Group_Project_/Assets/Scripts/test.cs
+++ Group_Project_/Assets/Scripts/test.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public static bool firstRight = true;
+    public static bool firstLeft = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,51 +17,69 @@ public class test : MonoBehaviour
             transform.Translate(1f * Time.deltaTime ,0 ,0);
         }*/
         
+           
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+
+        //> 11.5
+        //< -9
+        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < 11)
         {
             var swipeSpace = 0;
-            //while(swipeSpace < 1)
-            //{
-                //swipeSpace++;
-                //transform.Translate(transform.position.x ,0 ,0);
-                //playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-                var pos = transform.position;
-                pos.x += 0.5f;
-                transform.position = pos;
-            //}
+            
+            if(firstRight == true && firstLeft == true)
+            {
+                firstRight = false;
+                firstLeft = false;
+                while(swipeSpace < 250)
+                {
+                    swipeSpace++;
+                    var pos = transform.position;
+                    pos.x += 0.005f;
+                    transform.position = pos;
+                }
+                
+            }
+            else
+            {
+                while(swipeSpace < 500)
+                {
+                    swipeSpace++;
+                    var pos = transform.position;
+                    pos.x += 0.01f;
+                    transform.position = pos;
+                }
+            }
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > -8.5)
         {
             var swipeSpace = 0;
-            //while(swipeSpace < 1)
-            //{
-                //swipeSpace++;
-                //transform.Translate(transform.position.x ,0 ,0);
-                //playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            if(firstRight == true && firstLeft == true)
+            {
+                firstRight = false;
+                firstLeft = false;
                 var pos = transform.position;
-                pos.x -= 0.5f;
+                pos.x -= 2.5f;
                 transform.position = pos;
-            //}
+            }
+            else
+            {
+                var pos = transform.position;
+                pos.x -= 5f;
+                transform.position = pos;
+            }
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            var swipeSpace = 0;
-            //while(swipeSpace < 1)
-            //{
-                //swipeSpace++;
-                //transform.Translate(transform.position.x ,0 ,0);
-                //playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-                var pos = transform.position;
-                pos.z += 0.5f;
-                transform.position = pos;
-            //}
+            var pos = transform.position;
+            pos.z += 0.5f;
+            transform.position = pos;
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             var swipeSpace = 0;
             //while(swipeSpace < 1)
