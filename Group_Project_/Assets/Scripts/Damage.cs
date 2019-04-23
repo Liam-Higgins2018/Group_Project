723bea7a2a3ede67784de2fd8324c5c94bf5bf7c
Liam-Higgins2018 <liammhiggins@hotmail.com>
2019-04-23 16:12:12 +0100

    Damage Script added and implemented

---
+++ Group_Project_/Assets/Scripts/Damage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    //Gets trigger on colision
+    private void OnTriggerEnter()
+    {
+        GameObject.Find("Robot_Kyle").GetComponent<Health>().health -= 1;
+    }
+}
