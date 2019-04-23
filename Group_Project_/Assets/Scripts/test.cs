335f34755fa77ebb6be594b5298ddfdf567665fa
Jaymz95 <jamesmullarkey15@gmail.com>
2019-04-23 01:21:41 +0100

    more testing on translation of character object

--- Group_Project_/Assets/Scripts/test.cs
+++ Group_Project_/Assets/Scripts/test.cs
@@ -7,15 +7,26 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*var swipeSpace = 0;
+        while(swipeSpace < 500)
+        {
+            swipeSpace++;
+            transform.Translate(1f * Time.deltaTime ,0 ,0);
+        }*/
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            var swipeSpace = 0;
+            while(swipeSpace < 500)
+            {
+                swipeSpace++;
+                transform.Translate(1f * Time.deltaTime ,0 ,0);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*while()
-        {
-            transform.Translate(5f * Time.deltaTime ,0 ,0);
-        }*/
+        
     }
 }
