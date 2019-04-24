19269a339a89b731bc15c4739eed3a8c320e8fa0
Liam-Higgins2018 <liammhiggins@hotmail.com>
2019-04-24 15:36:36 +0100

    Conter and Difficulty Implemented

---
+++ Group_Project_/Assets/Scripts/Score.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private float score = 0.0f;
+    public Text textScore;
+    private int levelDifficulty = 1;
+    private int levelDifficultyMax = 10;
+    private int scoreToNextLevel = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(score >= scoreToNextLevel)
+        {
+            LevelUp();
+        }
+        score += Time.deltaTime;
+        textScore.text = ((int)score).ToString();
+    }
+
+    private void LevelUp()
+    {
+        if(levelDifficulty == levelDifficultyMax)
+        {
+            return;
+        }
+        scoreToNextLevel *= 2;
+        levelDifficulty++;
+
+        var run = transform.position;
+        run.z += 5.0f;
+        transform.position = run;
+    }
+}
